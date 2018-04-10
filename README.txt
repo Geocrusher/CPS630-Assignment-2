f9c01889e3fe5b5c64a4b4b249c1345d36d86c6e
soaapp <32276969+soaapp@users.noreply.github.com>
2018-04-10 10:41:52 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,23 @@
+# CPS630-Assignment-2
+CPS 630 Assignment 2 Iteration 2 - Group 42
+
+Members:
+Michelle Dervishi #500718934
+Sara Jahan #500748291
+Judel Villardo #500768474
+
+We have used PHP in this assignment, so we have to host it.
+Live Demo is needed to run this Iteration due to a 
+LOCAL DATABASE being run for the purposes of this project.
+Thank you, please refer to our report for some images as well.
+
+Tested Browser Support:
+Chrome
+Firefox
+MS Edge
+IE11
+
+Untested Browser Support:
+Opera
+Safari
+IE8 and older
--- viewer.php
+++ viewer.php
@@ -1,4 +1,5 @@
 <!DOCTYPE html>
+
 <html>
 
 <!-- This is the viewer window that displays art/artist information -->
@@ -10,6 +11,11 @@
   <!-- Import Stylesheets -->
   <link rel="stylesheet" type="text/css" href="viewer.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <!-- Import Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="scripts/display-lg.js"></script>
 </head>
 
 <body>
@@ -31,21 +37,17 @@
 
 </body>
 
-<!-- Import Scripts -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="scripts/display-lg.js"></script>
-
 <?php
-   $type = $_GET['type'];
-   $no = $_GET['no'];
+  $type = $_GET['type'];
+  $q = $_GET['no'];
 
-   if ($type == "art") {
-     echo "<script> artData($no) </script>";
-   } else {
-     echo "<script> artistData($no) </script>";
-   }
+  if ($type == "art") {
+    echo "<script> artData($q) </script>";
+  } else if ($type == "museum") {
+    echo "<script> museumData($q) </script>";
+  } else {
+    echo "<script> artistData($q) </script>";
+  }
 ?>
 
 </html>
